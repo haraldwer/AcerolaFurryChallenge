98baef0cea1b886b15e96574bf640ebb0e5fbaf6
Harald Wergeland <harwer009@gmail.com>
2023-11-17 21:07:06 +0100

    Revert "Deploying to main from @ haraldwer/AcerolaFurryChallenge@5c8e2e830e40735a7ae1120995d1aa700a4380c3 🚀"
    This reverts commit 23e8fca7fc906e2ae86968f42bd8727bbd0ae3f6.

---
+++ character.gd
@@ -0,0 +1,22 @@
+extends Node3D
+
+@export var Sensitivity = 0.005
+@export var Smoothing = 10.0
+
+var Dragging = false
+var TargetRot = Vector2(0.0, 0.0)
+var CurrentRot = Vector2(0.0, 0.0)
+
+func _ready():
+	pass
+
+func _process(delta):
+	Dragging = Input.is_action_pressed("drag")
+	CurrentRot = CurrentRot.lerp(TargetRot, delta * Smoothing)
+	$Character.rotation = Vector3(CurrentRot.y, CurrentRot.x, 0.0)
+	pass
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		if Dragging :
+			TargetRot += event.relative * Sensitivity
---
+++ shell.gd
@@ -0,0 +1,39 @@
+extends MeshInstance3D
+
+@export var MinInterp = 2.0
+@export var MaxInterp = 3.0
+@export var Fric = 0.0002
+@export var Gravity = 0.5
+
+var Velocity = Vector3()
+var Position = Vector3()
+var Reference : Node3D
+var Height
+var BaseVel = Vector3()
+
+func physics(delta) :
+	
+	global_rotation = Reference.global_rotation
+	
+	var targetPos = Reference.global_position
+	var difference = targetPos - Position
+	var posDelta = BaseVel + difference / delta
+	
+	# Gravity
+	Velocity.y -= Gravity * Height;
+	
+	# Acceleration
+	var interp = lerp(MinInterp, MaxInterp, Height)
+	Velocity = lerp(Velocity, posDelta, interp * delta)
+	
+	# Friction
+	Velocity *= pow(Fric, delta)
+	
+	# Apply velocity
+	Position += Velocity * delta
+	
+	global_position = lerp(targetPos, Position, Height)
+	
+	scale = Reference.scale
+	
+	pass
